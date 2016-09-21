b1d8b96976e9cf5daed5ee43f55782ce0ced0f01
Anisha Nagarajan <anishan@usc.edu>
2016-09-21 12:46:55 -0700

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Anisha Nagarajan on 9/13/16.
+//  Copyright © 2016 Anisha Nagarajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.22]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
+}
+
